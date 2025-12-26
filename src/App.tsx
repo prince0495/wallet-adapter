278486ffd7d951a64687f3bb2aa0342f57fa425b
Prince James Nagar <prince1.230101094@iiitbh.ac.in>
2025-12-26 17:10:06 +0530

    fixed for build version

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,7 @@ import { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
-    WalletModalProvider,
-    WalletDisconnectButton,
-    WalletMultiButton
+    WalletModalProvider
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
